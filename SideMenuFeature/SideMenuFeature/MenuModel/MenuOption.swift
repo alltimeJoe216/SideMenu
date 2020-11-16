d974ea2bb869048910690e88c0f712854df7e4f5
Joe V <joeveverka89@gmail.com>
2020-11-16 12:40:06 -0500

    Work.

---
+++ SideMenuFeature/SideMenuFeature/MenuModel/MenuOption.swift
@@ -0,0 +1,8 @@
+//
+//  MenuOption.swift
+//  SideMenuFeature
+//
+//  Created by Joseph Veverka on 11/16/20.
+//
+
+import Foundation
---
+++ SideMenuFeature/SideMenuFeature/View/MenuOptionTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MenuOptionTableViewCell.swift
+//  SideMenuFeature
+//
+//  Created by Joseph Veverka on 11/16/20.
+//
+
+import UIKit
+
+class MenuOptionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
